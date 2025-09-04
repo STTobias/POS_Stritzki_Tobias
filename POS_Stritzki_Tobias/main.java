8895b92db03ad1e6f3f16e9e385c7182507958c1
_CodeBlack_ <82730778+STTobias@users.noreply.github.com>
2025-09-04 17:47:33 +0200

    1.10

---
+++ POS_Stritzki_Tobias/main.java
@@ -0,0 +1,22 @@
+
+/**
+ * Beschreiben Sie hier die Klasse main.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class main
+{
+    private int x;
+    public main()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
