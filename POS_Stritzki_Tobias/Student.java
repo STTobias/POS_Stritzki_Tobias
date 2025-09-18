0c68df91e296be36d472a581084ad08409a5f9ea
_CodeBlack_ <82730778+STTobias@users.noreply.github.com>
2025-09-18 18:07:13 +0200

    1.14

--- POS_Stritzki_Tobias/Student.java
+++ POS_Stritzki_Tobias/Student.java
@@ -7,7 +7,7 @@ public class Student
     private int geburtsjahr;
     private char geschlecht;
 
-    public void setName(String name){
+    private void setName(String name) {
         if  (name.length() < 3) {
             throw new IllegalArgumentException("Der Name muss mindestens 3 Buchstaben haben!!!");
         } else if (name.length() > 50) {
@@ -16,23 +16,36 @@ public class Student
         this.name = name;
     }
 
-    public void setGeburtsjahr(int geburtsjahr){
+    private void setGeburtsjahr(int geburtsjahr) {
         this.geburtsjahr = geburtsjahr;
     }
 
-    public void setKg(float kilogramm){
+    private void setKg(float kilogramm) {
+        if (kilogramm > 635) {
+            throw new IllegalArgumentException("So schwer kannst du nicht sein!");
+        } else if (kilogramm < 2) {
+            throw new IllegalArgumentException("So leicht kannst du nicht sein!");
+        }
         this.kg = kilogramm;
     }
 
-    public void setCm(float centimeter){
+    private void setCm(float centimeter) {
+        if (centimeter > 250) {
+            throw new IllegalArgumentException("So groß kannst du nicht sein!");
+        } else if (centimeter < 50) {
+            throw new IllegalArgumentException("So klein kannst du nicht sein!");
+        }
         this.cm = centimeter;
     }
 
-    public void setGeschlecht(char geschlecht){
+    private void setGeschlecht(char geschlecht) {
+        if (geschlecht != 'm' && geschlecht != 'w') {
+            throw new IllegalArgumentException("Sry, Queers");
+        }
         this.geschlecht = geschlecht;
     }
 
-    public Student(String name, int geburtsjahr,float kg, float cm, char geschlecht){
+    public Student(String name, int geburtsjahr,float kg, float cm, char geschlecht) {
         this.setName(name);
         this.setGeburtsjahr(geburtsjahr);
         this.setKg(kg);
@@ -40,30 +53,29 @@ public class Student
         this.setGeschlecht(geschlecht);
     }
 
-    public void printStudent(){
+    public void printStudent() {
         System.out.println("Name: " + this.name + " (Geb. " + this.geburtsjahr + ")");
         System.out.println("BMI: " + bmi_rechner(this.kg, this.cm));
         System.out.println("Dein Geschlecht: " + MannOderFrau(this.geschlecht));
         System.out.println("Du bist: " + bmi_werter(this.geschlecht));
     }
 
-    public float bmi_rechner(float kg,float cm){
+    public float bmi_rechner(float kg,float cm) {
         cm /= 100;
         float ergebnis = (kg / (cm*cm));
         return ergebnis;
     }
 
-    public String MannOderFrau(char geschlecht){
+    public String MannOderFrau(char geschlecht) {
         if (geschlecht == 'm'){
             return "Männlich";
         }
-        else if (geschlecht == 'f'){
+        else {
             return "Weiblich";
         }
-        else return "Geben Sie 'm' oder 'f'";
     }
 
-    public String bmi_werter(char geschlecht){
+    public String bmi_werter(char geschlecht) {
         int normal_max_m = 25;
         int normal_min_m = 20;
         int normal_max_f = 24;
@@ -77,7 +89,7 @@ public class Student
             }
             else return "Normal";
         }
-        else if (geschlecht == 'f'){
+        else if (geschlecht == 'w'){
             if (bmi_rechner(this.kg, this.cm) < normal_min_f){
                 return "Iss was du kek";
             }
